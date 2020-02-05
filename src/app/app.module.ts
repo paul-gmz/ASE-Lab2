7378077c1c3ae2246481243377a0a5dd4ddccf8e
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-05 11:33:42 -0600

    user input to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from "./app.component";
 import { TopNavbarComponent } from "./top-navbar/top-navbar.component";
 import { WeatherComponent } from "./weather/weather.component";
 
 @NgModule({
-  declarations: [AppComponent, TopNavbarComponent, WeatherComponent],
-  imports: [BrowserModule],
+  declarations: [TopNavbarComponent, WeatherComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [TopNavbarComponent]
 })
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { createOfflineCompileUrlResolver } from "@angular/compiler";
 
 @Component({
-  selector: 'app-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  selector: "app-weather",
+  templateUrl: "./weather.component.html",
+  styleUrls: ["./weather.component.css"]
 })
 export class WeatherComponent implements OnInit {
+  city: string = "";
+  state: string = "";
 
-  constructor() { }
+  constructor() {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  getWeatherData(city, state): void {
+    console.log(city);
+    console.log(state);
   }
-
 }
